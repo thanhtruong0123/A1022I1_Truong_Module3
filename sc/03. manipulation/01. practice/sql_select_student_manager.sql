0853df5d92933264dd3071ae69ea515103bbcb50
thanhtruong0123 <thanhtruong0123@gmail.com>
2023-04-10 17:40:30 +0700

    05/04/23

--- sc/03. manipulation/01. practice/sql_select_student_manager.sql
+++ sc/03. manipulation/01. practice/sql_select_student_manager.sql
@@ -3,4 +3,11 @@ USE student_manager_db;
 SELECT * FROM student WHERE student_name
 LIKE "h%";
 
-SELECT * FROM class WHERE MONTH(start_date) >= 8;
+SELECT * FROM class WHERE MONTH(start_date) >= 12;
+
+SELECT * FROM subject WHERE credit
+BETWEEN 3 AND 5;
+
+UPDATE student
+SET class_id = 2
+WHERE student_name = 'Hung';

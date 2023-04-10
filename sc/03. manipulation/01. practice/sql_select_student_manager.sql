e1b5094edb6e19444a218057c0e53ff6459289aa
thanhtruong0123 <thanhtruong0123@gmail.com>
2023-04-10 18:08:57 +0700

    Manipulation

---
+++ sc/03. manipulation/01. practice/sql_select_student_manager.sql
@@ -0,0 +1,17 @@
+USE student_manager_db;
+
+SELECT * FROM student WHERE student_name
+LIKE "h%";
+
+SELECT * FROM class WHERE MONTH(start_date) >= 12;
+
+SELECT * FROM subject WHERE credit
+BETWEEN 3 AND 5;
+
+UPDATE student
+SET class_id = 2
+WHERE student_name = 'Hung';
+
+SELECT s.student_name, sub.sub_name, m.mark
+FROM student s JOIN mark m ON s.student_id = m.Student_id JOIN subject sub ON sub.sub_id=m.sub_id
+ORDER BY  m.mark DESC;
